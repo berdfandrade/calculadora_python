aa378724b3dee5b6a348f3c24035bf5d9fa5fc15
Bernardo Andrade <96706881+berdfandrade1@users.noreply.github.com>
2022-05-18 15:05:24 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,48 @@
+
+# Essa função adiciona
+def adiciona(x, y):
+    return x + y
+
+# Essa função subtrai
+
+def subtrai(x, y):
+    return x - y
+
+def multiplica(x, y):
+    return x * y
+
+def divide(x, y):
+    return x/ y
+
+print("Escolha a operação:")
+print("1. Adicionar")
+print("2. Subtrair")
+print("3. Multiplicar")
+print("4. Dividir")
+
+while True:
+    #Pega o valor escrito pelo usuario
+
+    escolha = input("Escola a operação(1/2/3/4): ")
+
+    if escolha in ('1', '2', '3', '4'):
+        num1 = int(input("Escolha o primeiro número: "))
+        num2 = int(input("Escolha o segundo número: "))
+
+    if escolha == '1':
+        print(num1, "-", num2, "=", subtrai(num1, num2))
+
+    elif escolha == '2':
+        print(num1, "*", num2, "=", multiplica(num1, num2))
+
+    elif escolha == '4':
+        print(num1, "/", num2, "=", divide(num1, num2))
+
+# Verificar se o usuario quer prosseguir com outra operacao
+
+    proximo_calc = input("Deseja fazer outra operacao? (sim/não): ")
+    if proximo_calc != "sim":
+        print("Tchau :)")
+        break
+
+else: print("Entrada Invalida")
